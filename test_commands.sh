044d0bdeb85db7969f7542f6bd203a726a3561d0
chiyoi <chiyoi2140@outlook.com>
2023-09-20 07:17:06 +0000

    tidy

--- test_commands.sh
+++ test_commands.sh
@@ -1,11 +1,11 @@
 ./osh
 echo nyan
 echo nyan | cat | cat | cat
-./bin/echo123
-./bin/echo123 | cat | cat | cat
+./echo123
+./echo123 | cat | cat | cat
 echo nyan &
-./bin/sleep_echo
-./bin/sleep_echo &
+./sleep_echo
+./sleep_echo &
 cd ..
 echo nyan > neko
 cat < neko < neko > neko1 > neko2
